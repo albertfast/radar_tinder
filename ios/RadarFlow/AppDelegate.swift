874ed33a509cf17fb6cf8c15cc62a61f5a2ee8b3
albertfast <ahmethasimsahiner@icloud.com>
2026-06-28 08:12:54 -0700

    updated app 123

--- ios/RadarFlow/AppDelegate.swift
+++ ios/RadarFlow/AppDelegate.swift
@@ -25,11 +25,7 @@ public class AppDelegate: ExpoAppDelegate {
 #if os(iOS) || os(tvOS)
     window = UIWindow(frame: UIScreen.main.bounds)
 // @generated begin @react-native-firebase/app-didFinishLaunchingWithOptions - expo prebuild (DO NOT MODIFY) sync-10e8520570672fd76b2403b7e1e27f5198a6349a
-if FirebaseApp.app() == nil {
-  if FirebaseApp.app() == nil {
-    FirebaseApp.configure()
-  }
-}
+FirebaseApp.configure()
 // @generated end @react-native-firebase/app-didFinishLaunchingWithOptions
     factory.startReactNative(
       withModuleName: "main",
